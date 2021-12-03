b4186f06c3ca0add2e5f4960c7ec8f0bfcaa4b02
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-12-02 20:53:58 -0500

    recipes seeded and routes set up for /recipes

--- controllers/api/recipe-routes.js
+++ controllers/api/recipe-routes.js
@@ -2,11 +2,16 @@ const router = require('express').Router();
 const Recipe = require('../../models/index.js');
 const sequelize = require('../../config/connection');
 
-
-router.get('/recipes', (req, res) => {
+// localhost:3001/api/recipes
+router.get('/', (req, res) => {
   Recipe.findAll({
     attributes: ['id', 'title', 'ing_1', 'ing_2', 'ing_3']
-  });
+  })
+    .then((recipeData) => res.json(recipeData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
