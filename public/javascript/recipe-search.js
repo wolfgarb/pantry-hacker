76f1e3de62fc99d8a8544602185be0602376c5e3
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-12-05 15:17:54 -0500

    add form for searching ingredients, set up front end js for form

---
+++ public/javascript/recipe-search.js
@@ -0,0 +1,13 @@
+function recipeSearch(event) {
+  event.preventDefault();
+
+  const ing_1 = document.querySelector('input[name="ing-1"]').value.trim();
+  const ing_2 = document.querySelector('input[name="ing-2"]').value.trim();
+  const ing_3 = document.querySelector('input[name="ing-3"]').value.trim();
+
+  console.log(ing_1 + ing_2 + ing_3);
+}
+
+document
+  .querySelector('.recipe-search-form')
+  .addEventListener('submit', recipeSearch);
