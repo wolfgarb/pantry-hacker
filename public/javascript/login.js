15e069bec9d4f97e91aa0a4bccf2b1a913da0cda
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-12-10 11:45:31 -0500

    quick-save

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,26 +1,27 @@
 async function loginFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
 
-        if (response.ok) {
-            document.location.replace('/dashboard/');
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert('incorrect login credentials');
     }
+  }
 }
 
-
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
