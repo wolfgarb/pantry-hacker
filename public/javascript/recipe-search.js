742a7bb72d30c712b2df17af2a68609d90826ec8
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-12-09 19:08:59 -0500

    quick-save

--- public/javascript/recipe-search.js
+++ public/javascript/recipe-search.js
@@ -1,17 +1,10 @@
-async function recipeSearch(event) {
+function recipeSearch(event) {
   event.preventDefault();
+  const ing_1 = document.querySelector('input[name="ing-1"]').value.trim();
+  const ing_2 = document.querySelector('input[name="ing-2"]').value.trim();
+  const ing_3 = document.querySelector('input[name="ing-3"]').value.trim();
 
-  const ingList = document.querySelector('input[name="ing-list"').value;
-  console.log(ingList);
-
-  const response = await fetch(`/api/recipes/${ingList}`);
-
-  if (response.ok) {
-    document.location.replace('/');
-    console.log(response);
-  } else {
-    alert(response.statusText);
-  }
+  console.log(ing_1 + ing_2 + ing_3);
 }
 
 document
@@ -21,5 +14,13 @@ document
 // const ing_1 = document.querySelector('input[name="ing-1"]').value.trim();
 // const ing_2 = document.querySelector('input[name="ing-2"]').value.trim();
 // const ing_3 = document.querySelector('input[name="ing-3"]').value.trim();
-
 // console.log(ing_1 + ing_2 + ing_3);
+
+// const response = await fetch(`/api/recipes/`);
+
+// if (response.ok) {
+//   document.location.replace('/');
+//   console.log(response);
+// } else {
+//   alert(response.statusText);
+// }
